e9fcce884f01e184830e8817db1eb1c57183f838
Aravind Sridharan <96038018+IndianJohnnyDepp@users.noreply.github.com>
2023-09-26 17:04:37 +0530

    Update predict.js

--- predict.js
+++ predict.js
@@ -11,3 +11,22 @@ $file.addEventListener("change", (event) => {
       fileReader.readAsDataURL(imageFile);
     }
   });
+
+import axios from 'axios'
+const requestToAWS=axios({
+   method: 'post',
+   url: ' https://i4s4lnf9bb.execute-api.eu-central-1.amazonaws.com/allowed_ip/predict-pneumonia',
+   headers: {'Content-type': 'application/x-image'},
+   data:srcdata})
+  .then((resp) => {
+    return resp.data;
+  }).catch(err => {
+  console.log('Error', err);
+  });
+
+  const respFromAWS = await requestToAWS;
+  return res.json({ message: respFromAWS});
+
+   } catch (err) {
+  console.log(err)}
+};
