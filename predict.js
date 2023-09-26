fed8458cf32b8627fa74b2621d5996e6cb895bc7
IndianJohnnyDepp <aravind.14may93@gmail.com>
2023-09-26 13:26:16 +0530

    Seventh_26.09.23_1326

--- predict.js
+++ predict.js
@@ -1,35 +1,29 @@
-import axios from 'axios'
+const input = document.getElementById("selectAvatar");
+const avatar = document.getElementById("avatar");
+const textArea = document.getElementById("textArea");
 
-export const predictImage = async (req, res) => {
-  try {
-    const { image } = req.body;
+const convertBase64 = (file) => {
+  return new Promise((resolve, reject) => {
+    const fileReader = new FileReader();
+    fileReader.readAsDataURL(file);
 
-    console.log(image)
+    fileReader.onload = () => {
+      resolve(fileReader.result);
+    };
 
-    if (!image) return res.status(400).send("No image");
-    let cleansedImage=image.replace(/^data:image\/\w+;base64,/, "")
-
-    let b64string = cleansedImage
-    //Buffer objects are used to represent a fixed-length sequence of bytes
-    let buffer = Buffer.from(b64string, 'base64');
-
-
- const requestToAWS=axios({
-   method: 'post',
-   url: ' https://bwlwkhjj28.execute-api.ap-south-1.amazonaws.com/Production/predict-pneumonia',
-   headers: {
-     'Content-type': 'application/x-image'
-   },
-   data:buffer})
-  .then((resp) => {
-    return resp.data;
-  }).catch(err => {
-  console.log('Error', err);
+    fileReader.onerror = (error) => {
+      reject(error);
+    };
   });
-
-  const respFromAWS = await requestToAWS;
-  return res.json({ message: respFromAWS});
-
-   } catch (err) {
-  console.log(err)}
 };
+
+const uploadImage = async (event) => {
+  const file = event.target.files[0];
+  const base64 = await convertBase64(file);
+  avatar.src = base64;
+  textArea.innerText = base64;
+};
+
+input.addEventListener("change", (e) => {
+  uploadImage(e);
+});
